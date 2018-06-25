73e4d6874898622a31fef446d9ddb2059d70fb84
wincorp <tom@wincorp.net>
2018-06-25 10:55:18 -0700

    add cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const config = require('./config');
 
@@ -18,6 +19,7 @@ mongoose.connect(config.database, (err) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req, res, next) => {
     res.json({
