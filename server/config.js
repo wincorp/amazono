ec1825f17800d82640a41750defd147971810a97
wincorp <tom@wincorp.net>
2018-06-25 10:43:03 -0700

    setup server.js

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://tmargraff:Sapphire5211@ds217671.mlab.com:17671/amazonowebapplication',
+    port: 3030,
+    secret: "tmargraff"
+}
--- server/server.js
+++ server/server.js
@@ -1,1 +1,30 @@
-// server 
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+
+const app = express();
+
+mongoose.connect(config.database, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("connected to the database");
+    }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
+
+app.get('/', (req, res, next) => {
+    res.json({
+        user: "tom margraff"
+    });
+})
+
+app.listen(config.port, (err) => {
+    console.log("Magic happens on port " + config.port);
+})
